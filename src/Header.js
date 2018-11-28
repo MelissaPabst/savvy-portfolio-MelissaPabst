e7d3a20928f8d1ce89427604efece52abcfb4cee
Melissa Pabst <melissapabst@gmail.com>
2018-11-27 19:44:41 -0600

    fixes exposed html comment

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,7 @@ export default function Header(state) {
     return `
         <div id="header">
             <div class="container">
-                // access the title property of state with dot notation
+                <!--access the title property of state with dot notation-->
                 <h1>${state.title}</h1>
                 <h2>Me llamo es Melissa Pabst</h2>
                 <img src="https://avatars2.githubusercontent.com/u/29720087?s=400&u=462b05241a936aa8c97bdaec504b6a937c576805&v=4"
