34b5a37a0e3f622b0e9660e84ec008ecf3b1028e
Melissa Pabst <melissapabst@gmail.com>
2018-12-11 20:04:35 -0600

    remove greeting.js

--- src/Greeting.js
+++ src/Greeting.js
@@ -1,27 +1,27 @@
-var userName = prompt("WhAt Is yOUr nAmE?");
+// var userName = prompt("WhAt Is yOUr nAmE?");
 
 
 
-export default function greetUser() {
+// export default function greetUser() {
 
-    var title = document.querySelector('h1');
+//     var title = document.querySelector('h1');
 
-    if (!userName) {
-        userName = prompt("Hello, user. Please give me your name");
+//     if (!userName) {
+//         userName = prompt("Hello, user. Please give me your name");
 
-        greetUser();
-    } 
-    else {
-        //else only called in the event we have the username (a non-empty string)
-        //alert("Hello, " + userName);
-        //instead of alert, make greeting appear in h1
-        //object.key.value
-        //title.textContent = "Hello, " + userName;
-        //title.textContent = title.textContent + ', ' + userName;
-        // title.textContent += ', ' + userName;
-        title.textContent += `, ${userName}`;
-        // title.innerHTML += `, <em>${userName}</em>`;
-    }
-};
-// remember to invoke greetUser() in index.js
-// greetUser();
+//         greetUser();
+//     } 
+//     else {
+//         //else only called in the event we have the username (a non-empty string)
+//         //alert("Hello, " + userName);
+//         //instead of alert, make greeting appear in h1
+//         //object.key.value
+//         //title.textContent = "Hello, " + userName;
+//         //title.textContent = title.textContent + ', ' + userName;
+//         // title.textContent += ', ' + userName;
+//         title.textContent += `, ${userName}`;
+//         // title.innerHTML += `, <em>${userName}</em>`;
+//     }
+// };
+// // remember to invoke greetUser() in index.js
+// // greetUser();
