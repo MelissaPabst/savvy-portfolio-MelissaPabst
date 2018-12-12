9120efc08e0043c67da9049e9ebc634a5730d5bf
Melissa Pabst <melissapabst@gmail.com>
2018-12-11 20:51:46 -0600

    refactor for loop in navigation.js into for each

--- src/Navigation.js
+++ src/Navigation.js
@@ -3,24 +3,44 @@ import { lowerCase } from 'lodash';
 
 function buildLinks(links){
     var list = '';
-    var link = '';
+    // var link = '';
     
-    for(let i = 0; i < links.length; i++){
-        if(links[i] !== 'Home'){
-            link = links[i];
+    links.forEach((link) => {
+        var href = '';
+
+        if(link !== 'Home'){
+            href = link;
         }
         list += `
             <li>
-                <a href="/${lowerCase(link)}" data-navigo>
-                    ${links[i]}
+                <a href="/${href}" data-navigo>
+                    ${link}
                 </a>
             </li>
         `;
+    });
 
-        // console.log(link); for funsies!
-    }
 
-    // console.log(list);
+
+    
+
+
+    // for(let i = 0; i < links.length; i++){
+    //     if(links[i] !== 'Home'){
+    //         link = links[i];
+    //     }
+    //     list += `
+    //         <li>
+    //             <a href="/${lowerCase(link)}" data-navigo>
+    //                 ${links[i]}
+    //             </a>
+    //         </li>
+    //     `;
+
+    //     // console.log(link); for funsies!
+    // }
+
+    // // console.log(list);
     return list;
 
 }  
